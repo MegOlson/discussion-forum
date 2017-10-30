1af9c089768566d33f2d3931d7c8f9fb7426d0d5
Megan Olson <meganannetteolson@yahoo.com>
2017-10-30 10:10:44 -0700

    Add list of hard coded post objects to all page.

--- src/app/all/all.component.ts
+++ src/app/all/all.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Post } from '../post.model';
+
 
 @Component({
   selector: 'app-all',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./all.component.css']
 })
 export class AllComponent {
+  @Output() clickSender = new EventEmitter();
 
+  masterPostList: Post[] = [
+    new Post("Kristen Bell is Elsa?","Kristen Bell, the voice of Anna in Frozen, had to be Elsa for Halloween, because her daughter made her do it."),
+    new Post("Finnish Lapland trees covered in snow","Finnish Lapland trees covered in snow and it was incredible."),
+    new Post("Saying goodbye","It is very hard to say goodbye so just skip goodbyes and leave.")
+  ]
 }
