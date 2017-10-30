f25c4de33ca7f4d3c1b54995aefb5f76f24e2181
Megan Olson <meganannetteolson@yahoo.com>
2017-10-30 11:42:00 -0700

    Commit before lunch.

--- src/app/all/all.component.ts
+++ src/app/all/all.component.ts
@@ -11,8 +11,15 @@ export class AllComponent {
   @Output() clickSender = new EventEmitter();
 
   masterPostList: Post[] = [
-    new Post("Kristen Bell is Elsa?","Kristen Bell, the voice of Anna in Frozen, had to be Elsa for Halloween, because her daughter made her do it."),
+    new Post("Elsa?","Kristen Bell, the voice of Anna in Frozen, had to be Elsa for Halloween, because her daughter made her do it."),
     new Post("Finnish Lapland trees covered in snow","Finnish Lapland trees covered in snow and it was incredible."),
     new Post("Saying goodbye","It is very hard to say goodbye so just skip goodbyes and leave.")
   ]
+
+  addPostChild(newPost: Post){
+   this.masterPostList.push(newPost);
+ }
+  editButtonClicked(postToEdit: Post) {
+    this.clickSender.emit(postToEdit);
+  }
 }
--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
+
 export class EditComponent {
+  @Input() childSelectedPost: Post;
 }
